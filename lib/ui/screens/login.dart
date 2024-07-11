9ab17687e51811bf0aea1b241df34ebb330bc03f
osmaldym <osmaldy11checo@gmail.com>
2024-07-11 16:49:24 -0400

    Adding some elements to login page by components and others

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/assets/AppTheme.dart';
+import 'package:wallet/ui/widgets/footer.dart';
+import 'package:wallet/ui/widgets/input.dart';
 
 class Login extends StatelessWidget{
   const Login({ super.key });
@@ -10,9 +13,58 @@ class Login extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image(image: AssetImage('assets/logo.png'))
+            const Image(image: AssetImage('assets/logo.png')),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 50
+              ),
+              child: Column(
+                children: <Widget>[
+                  CInput(
+                    focus: true,
+                  ),
+                  CInput(
+                    type: Types.pass
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: (){},
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 25
+                          ),
+                          child: Text(
+                            'Olvidé mi contraseña',
+                            style: TextStyle(
+                              fontSize: 14
+                            ),
+                            textAlign: TextAlign.end,
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Divider(
+                          color: AppTheme.of(context).contrast
+                        )
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+      bottomSheet: CFooter(
+        onPressedBtn: (){},
+        btnText: "Iniciar sesión",
       ),
     );
   }
