5a6db0a87539d36a4dde5d826db9941f945a7537
osmaldym <osmaldy11checo@gmail.com>
2024-09-16 00:36:50 -0400

    Adding sign in/login (google) options and circle logo

--- lib/ui/screens/login.dart
+++ lib/ui/screens/login.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:wallet/assets/AppTheme.dart';
 import 'package:wallet/ui/widgets/footer.dart';
@@ -5,15 +8,28 @@ import 'package:wallet/ui/widgets/input.dart';
 
 class Login extends StatelessWidget{
   const Login({ super.key });
-  
+
   @override
   Widget build(BuildContext context){
+    double googleBtnSize = Platform.isIOS ? 44*1.2 : 40*1.2;
+    double googleImgSize = 20;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Image(image: AssetImage('assets/logo.png')),
+             const CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 100,
+              child: Padding(
+                padding: EdgeInsets.all(35),
+                  child: Image(
+                  image: AssetImage('assets/logo.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 30,
@@ -34,7 +50,7 @@ class Login extends StatelessWidget{
                         onTap: (){},
                         child: const Padding(
                           padding: EdgeInsets.symmetric(
-                            vertical: 25
+                            vertical: 15
                           ),
                           child: Text(
                             'Olvidé mi contraseña',
@@ -55,7 +71,49 @@ class Login extends StatelessWidget{
                         )
                       )
                     ],
-                  )
+                  ),
+                  OutlinedButton( // Google button
+                    onPressed: (){},
+                    style: OutlinedButton.styleFrom(
+                      minimumSize: Size(googleBtnSize, googleBtnSize),
+                      padding: EdgeInsets.zero,
+                      backgroundColor: Color(
+                        MediaQuery.of(context).platformBrightness == Brightness.dark ? 0xFF131314 : 0xFFFFFFFF
+                      ) 
+                    ),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: <Widget>[
+                        Image(
+                          width: googleImgSize,
+                          height: googleImgSize,
+                          image: const AssetImage("assets/brand_logos/google.png")
+                        ),
+                      ],
+                    )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          const TextSpan(
+                            text: "¿No tienes cuenta? "
+                          ),
+                          TextSpan(
+                            text: "Registrate",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold
+                            ),
+                            recognizer: TapGestureRecognizer()..onTap = () {
+                              // Add function
+                            }
+                          )
+                        ]
+                      )
+                    ),
+                  ),
                 ],
               ),
             ),
