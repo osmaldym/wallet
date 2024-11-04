1c17dd60fa46f70bd60fd4c406b65a54883abf34
osmaldym <osmaldy11checo@gmail.com>
2024-11-04 00:56:29 -0400

    Creating AppRoutes const to set all app routes with automatization methods

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../modules/auth/login/login_page.dart';
+import '../../modules/auth/signin/signin_page.dart';
+
+class AppRoutes {
+  /// Pages routes
+  static final GoRouter pages = GoRouter(
+    routes: <RouteBase>[
+      _newRoot(
+        const Login(),
+        routes: [
+          _newRoute('login', const Login()),
+        ]
+      )
+    ]
+  );
+
+  /// Set's routes more easier
+  static GoRoute _newRoute(String url, Widget page, { List<RouteBase> childs = const <RouteBase>[] }){
+    return GoRoute(
+      path: url,
+      builder: (context, state) => page,
+      routes: childs
+    );
+  }
+
+  /// Set the root route
+  static GoRoute _newRoot(Widget page, { String redirect = "", List<RouteBase> routes = const <RouteBase>[] }){
+    return GoRoute(
+      path: "/",
+      builder: (context, state) => page,
+      redirect: (context, state) => redirect != "" ? redirect : null,
+      routes: routes,
+    ); 
+  }
+}
