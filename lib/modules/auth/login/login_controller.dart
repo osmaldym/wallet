567475d246e04b0bd3ed83f00869b3b0cb5dda85
osmaldym <osmaldy11checo@gmail.com>
2024-11-05 00:45:18 -0400

    Adding login method for logIn btn and upgrading goToSignin method in login controller

--- lib/modules/auth/login/login_controller.dart
+++ lib/modules/auth/login/login_controller.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class LoginController {
-  Future<void> goToSignin(BuildContext context) => context.push("/signin");
+  void goToSignin(BuildContext context) => context.push("/signin");
+
+  void login(BuildContext context) {
+    context.go("/");
+  }
 }
