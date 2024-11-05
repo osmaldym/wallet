c098a2d084188defc523bb4b0ee26635a589ee9b
osmaldym <osmaldy11checo@gmail.com>
2024-11-05 00:42:15 -0400

    Setting home page the main route, login route to initialLocation and upgrading _newRoot method

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:wallet/modules/home/home_page.dart';
 
 import '../../modules/auth/login/login_page.dart';
 import '../../modules/auth/signin/signin_page.dart';
@@ -7,9 +8,10 @@ import '../../modules/auth/signin/signin_page.dart';
 class AppRoutes {
   /// Pages routes
   static final GoRouter pages = GoRouter(
+    initialLocation: '/login',
     routes: <RouteBase>[
       _newRoot(
-        const Login(),
+        const Home(),
         routes: [
           _newRoute('login', const Login()),
           _newRoute('signin', const Signin()),
@@ -26,13 +28,11 @@ class AppRoutes {
       routes: childs
     );
   }
-
   /// Set the root route
-  static GoRoute _newRoot(Widget page, { String redirect = "", List<RouteBase> routes = const <RouteBase>[] }){
+  static GoRoute _newRoot(Widget page, { List<RouteBase> routes = const <RouteBase>[] }){
     return GoRoute(
       path: "/",
       builder: (context, state) => page,
-      redirect: (context, state) => redirect != "" ? redirect : null,
       routes: routes,
     ); 
   }
