cc1cd57f7fd420e5d45ba281e2512047e636099d
osmaldym <osmaldy11checo@gmail.com>
2025-04-30 22:57:10 -0400

    New component upcoming menu options with all necessary

---
+++ lib/modules/shared/widgets/menuOption.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:wallet/core/constants/theme/AppTheme.dart';
+
+class MenuOption extends ListTile {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  String text;
+  IconData icon;
+  double size;
+  void Function() onTap;
+
+  MenuOption({
+    super.key,
+    required this.onTap,
+    this.text = "Home",
+    this.icon = Icons.home,
+    this.size = 32,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      key: _scaffoldKey,
+      title: Text(text),
+      onTap: onTap,
+      leading: Icon(icon, size: size),
+      iconColor: AppTheme.of(context).textContrast,
+      textColor: AppTheme.of(context).textContrast,
+      splashColor: Colors.grey,
+    );
+  }
+}
