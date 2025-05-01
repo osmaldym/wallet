0fa4a24e9d606dd26d26cc14b0d224b53a007d6d
osmaldym <osmaldy11checo@gmail.com>
2025-04-30 23:00:06 -0400

    New menu component with all necessary

---
+++ lib/modules/shared/widgets/menu.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wallet/core/constants/app_images.dart';
+import 'package:wallet/core/constants/theme/AppTheme.dart';
+import 'package:wallet/modules/shared/widgets/menuOption.dart';
+
+class Menu extends Drawer {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  double width;
+
+  Menu({
+    super.key,
+    this.width = 270,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      key: _scaffoldKey,
+      width: width,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+              color: AppTheme.of(context).primary
+            ),
+            height: 150,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15
+            ),
+            child: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 25),
+                  child: Image(
+                    height: 50,
+                    image: AssetImage(AppImages.appLogo),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Text(
+                  "Wallet App",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18
+                  ),
+                )
+              ],
+            )
+          ),
+          MenuOption(
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Next pays",
+            icon: Icons.money,
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Records",
+            icon: Icons.storage_rounded,
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Statistics",
+            icon: Icons.trending_up,
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Goals",
+            icon: Icons.sports_score,
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Budgets",
+            icon: Icons.calculate,
+            onTap: () => context.go("/"),
+          ),
+          MenuOption(
+            text: "Settings",
+            icon: Icons.settings,
+            onTap: () => context.go("/"),
+          )
+
+        ],
+      ),
+    );
+  }
+}
