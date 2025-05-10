095cf8783cea29b0f48de08aef0cf37380eabd0e
osmaldym <osmaldy11checo@gmail.com>
2025-05-10 00:14:30 -0400

    Adding missing nullable properties in convertions

--- lib/core/utils/convertions.dart
+++ lib/core/utils/convertions.dart
@@ -4,14 +4,14 @@ import 'package:wallet/modules/shared/drivers/local/models/user.dart';
 class Convertions {
   static List<User> responseToUserList(List<Map<String, Object?>> response) {
     return [
-      for (final { 'id': id as int, 'server_id': serverId as int, 'names': names as String, 'email': email as String, 'img': img as String, 'password': password as String } in response)
+      for (final { 'id': id as int, 'server_id': serverId as int?, 'names': names as String, 'email': email as String?, 'img': img as String?, 'password': password as String? } in response)
         User(id: id, serverId: serverId, email: email, img: img, password: password, names: names)
     ];
   }
 
   static List<Account> responseToAccountList(List<Map<String, Object?>> response) {
     return [
-      for (final { 'id': id as int, 'server_id': serverId as int, 'user_id': userId as int, 'title': title as String } in response)
+      for (final { 'id': id as int, 'server_id': serverId as int?, 'user_id': userId as int, 'title': title as String } in response)
         Account(id: id, serverId: serverId, userId: userId, title: title)
     ];
   }
