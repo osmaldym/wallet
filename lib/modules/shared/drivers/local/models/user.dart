0317ce55c33d7f347a763459715e40df355d8bf4
osmaldym <osmaldy11checo@gmail.com>
2025-05-09 23:03:14 -0400

    New models account/user for local db responses

---
+++ lib/modules/shared/drivers/local/models/user.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final int ?id;
+  final int ?serverId;
+  final String ?email;
+  final String ?password;
+  final String ?img;
+
+  const User({
+    this.id,
+    this.serverId,
+    this.email,
+    this.password,
+    this.img,
+  });
+
+  Map<String, Object?> toMap() {
+    return { 'id': id, 'server_id': serverId, 'email': email, 'img': img, 'password': password };
+  }
+
+  @override
+  String toString(){
+    String toRet = "User {";
+    for(var entry in toMap().entries) toRet += entry.key + ': ' + entry.value.toString();
+    toRet += "}";
+    return toRet;
+  }
+}
