b206d8c375313f03b98de36da428baecf275cdcc
osmaldym <osmaldy11checo@gmail.com>
2025-05-22 21:54:03 -0400

    New param, isLoading

--- lib/modules/shared/widgets/fragments/button.dart
+++ lib/modules/shared/widgets/fragments/button.dart
@@ -4,12 +4,14 @@ import 'package:wallet/core/constants/theme/app_theme.dart';
 class CButton extends StatelessWidget {
   String? text;
   Color? bgColor;
+  bool? isLoading;
   void Function() onPressed;
 
   CButton({
     super.key,
     this.text = '',
     this.bgColor,
+    this.isLoading,
     required this.onPressed
   });
 
@@ -19,15 +21,23 @@ class CButton extends StatelessWidget {
     Color bgCol = bgColor ?? AppTheme.of(context).primary;
 
     return TextButton(
-      onPressed: onPressed,
+      onPressed: isLoading != null && isLoading! ? null : onPressed,
       style: TextButton.styleFrom(
         backgroundColor: bgCol,
         minimumSize: const Size.fromHeight(50),
+        disabledBackgroundColor: bgCol.withAlpha(150),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10))
         )
       ),
-      child: Text(
+      child: isLoading != null && isLoading! ? SizedBox(
+        height: 20,
+        width: 20,
+        child: CircularProgressIndicator(
+          strokeWidth: 2.5,
+          color: AppTheme.of(context).textContrast
+        ),
+      ) : Text(
         txt,
         style: TextStyle(
           color: AppTheme.of(context).contrast,
