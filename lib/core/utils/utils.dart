e74d7a0302830d372d78b49bff256cfc1fc65308
osmaldym <osmaldy11checo@gmail.com>
2025-05-26 22:28:30 -0400

    New methods for readable date and readable datetime

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Utils {
+  final DateFormat _readableDateFormat = DateFormat("dd/MM/yyyy");
+  final DateFormat _readableTimeFormat = DateFormat("hh:mm a");
+
   /// Converts a DateTime with TimeOfDay
   /// 
   /// if [datetime] doesn't passed it returns null
@@ -11,4 +15,10 @@ class Utils {
     time ??= const TimeOfDay(hour: 0, minute: 0);
     return DateTime(datetime.year, datetime.month, datetime.day, time.hour, time.minute);
   }
+
+  String toReadableDate(DateTime datetime) => _readableDateFormat.format(datetime);
+
+  String toReadableDateTime(DateTime datetime) => "${_readableDateFormat.format(datetime)} ${_readableTimeFormat.format(datetime)}";
+
+  String toReadableTime(DateTime datetime) => _readableTimeFormat.format(datetime);
 }
