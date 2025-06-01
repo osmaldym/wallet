600312e27566d021e02d472ac64780f1c22f9969
osmaldym <osmaldy11checo@gmail.com>
2025-06-01 19:16:59 -0400

    Now select of accounts shows the ceated accounts, and updating another empty selects

--- lib/modules/scheduled_pays/put/put_controller.dart
+++ lib/modules/scheduled_pays/put/put_controller.dart
@@ -1,10 +1,15 @@
 import 'package:wallet/modules/shared/drivers/local/dao.dart';
+import 'package:wallet/modules/shared/drivers/local/models/account.dart';
 import 'package:wallet/modules/shared/drivers/local/models/scheduled_pay.dart';
 
 class PutController {
-  late Dao daoLocal = Dao();
+  late Dao dao = Dao();
 
   Future<void> createPay(ScheduledPay pay) async {
-    await daoLocal.putScheduledPay(pay.toMap());
+    await dao.putScheduledPay(pay.toMap());
+  }
+
+  Future<List<Account>> getAllAccounts() async {
+    return await dao.accounts();
   }
 }
