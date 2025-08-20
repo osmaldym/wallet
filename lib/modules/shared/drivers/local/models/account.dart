736195ec4aaf0ba4a63690fbdb72bc11b28ef011
osmaldym <osmaldy11checo@gmail.com>
2025-08-19 23:29:22 -0400

    Adding toString to amount property

--- lib/modules/shared/drivers/local/models/account.dart
+++ lib/modules/shared/drivers/local/models/account.dart
@@ -22,7 +22,7 @@ class Account extends Defs {
     'server_id': serverId,
     'user_id': userId,
     'title': title,
-    'amount': amount,
+    'amount': amount?.toString(),
     'is_total': isTotal.boolToIntOrNull(),
   };
 
