7fb85676e8b4ad10caa1f69af4eaab31c8aca0d5
osmaldym <osmaldy11checo@gmail.com>
2025-11-29 23:04:14 -0500

    Returning null if cannot get the public IP in getPublicIp

--- lib/modules/shared/drivers/http/web_dao.dart
+++ lib/modules/shared/drivers/http/web_dao.dart
@@ -1,7 +1,11 @@
 import 'package:dart_ipify/dart_ipify.dart';
 
 class WebDao {
-  Future<String> getPublicIp() async {
-    return await Ipify.ipv4();
+  Future<String?> getPublicIp() async {
+    try {
+      return await Ipify.ipv4();
+    } catch (e) {
+      return null;
+    }
   }
 }
