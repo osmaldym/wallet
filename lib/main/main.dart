4e317b36d6388945eaf91ebdc8a3a1384ab36d78
osmaldym <osmaldy11checo@gmail.com>
2026-01-31 21:49:14 -0500

    Sending notifications about copying the data of dabatase and sending this copy to a pc backup

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,23 +1,80 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 import 'package:wallet/core/constants/app_routes.dart';
 import 'package:wallet/core/constants/theme/app_theme.dart';
 import 'package:wallet/core/providers/LocaleNotifier.dart';
 import 'package:wallet/core/utils/LocalData.dart';
+import 'package:wallet/modules/shared/drivers/local/dao.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() => runApp(
-  MultiProvider(providers: [ 
-    /*
-     If is created a new provider, set in this array depends if 
-     is ChangeNotifierProvider, only Provider, etc
-    */
-    ChangeNotifierProvider(create: (context) => LocaleNotifier())
-  ],
-  child: const MyApp())
-);
+@pragma('vm:entry-point')
+void callbackDispacher(){
+  Workmanager().executeTask((task, inputData) async {
+    return Future.value(true);
+  });
+}
+
+bool dataSendedForTheLast15Minutes = false;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Dao dao = Dao();
+
+  Workmanager().initialize(
+    callbackDispacher,
+    isInDebugMode: true,
+  );
+
+  Workmanager().registerPeriodicTask(
+    "Task 1",
+    "Task of create db registers locally and in server in the last 15 minutes: $dataSendedForTheLast15Minutes",
+    frequency: const Duration(minutes: 15)
+  );
+
+  Workmanager wm = Workmanager();
+    Timer.periodic(const Duration(seconds: 30), (Timer t) async {
+      try {
+        final resp = await post(
+          Uri.parse("http://192.168.8.171:8000"),
+          headers: <String, String> {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(await dao.getAllData())
+        );
+
+        if (resp.statusCode < 300 && resp.statusCode >= 200) {
+          // Successful POST request
+          print("SENDED");
+          // wm.registerOneOffTask("Data sended", "Data sended to the database of server");
+          dataSendedForTheLast15Minutes = true;
+        } else {
+          // Handle errors
+          wm.registerOneOffTask("Data no sended", "Error sending data to the database of server");
+          dataSendedForTheLast15Minutes = false;
+        }
+      } catch (e) {
+        print(e);
+      }
+    });
+
+  runApp(
+    MultiProvider(providers: [ 
+      /*
+      If is created a new provider, set in this array depends if 
+      is ChangeNotifierProvider, only Provider, etc
+      */
+      ChangeNotifierProvider(create: (context) => LocaleNotifier())
+    ],
+    child: const MyApp())
+  );
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
