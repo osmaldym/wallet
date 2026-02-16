8f78ec6a34fcd9f0ae858506df7f979bec1b4f6c
osmaldym <osmaldy11checo@gmail.com>
2026-02-15 21:59:13 -0500

    new goals page/controller only with FloatingActionButton

---
+++ lib/modules/goals/goals_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:wallet/core/constants/app_route.dart';
+import 'package:wallet/modules/shared/widgets/header.dart';
+import 'package:wallet/core/utils/app_localizations_x.dart';
+
+class GoalsPage extends StatefulWidget {
+  GoalsPage({
+    super.key,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _GoalsPageState();
+}
+
+class _GoalsPageState extends State<GoalsPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CHeader(
+        title: context.l10n!.goals,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => print("Hello world!"),
+      ),
+    );
+  }
+}
